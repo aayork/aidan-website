0adb29973fb5d47bc20ab715c41962e80d2676d2
Aidan York <york_aa19@outlook.com>
2023-02-13 10:37:38 -0500

    Project 1 Work

---
+++ nmc/4020/Project-1/js/project.js
@@ -0,0 +1,59 @@
+let arrayOne = [
+  "Jeb Bush",
+  "Ben Carson",
+  "Lincoln Chafee",
+  "Chris Christie",
+  "Ted Cruz",
+  "Carly Fiorina",
+  "Jim Gilmore",
+];
+
+let arrayTwo = [
+  "Lindsey Graham",
+  "Bobby Jindal",
+  "John Kasich",
+  "Lawrence Lessig",
+  "Martin O'Malley",
+  "George Pataki",
+  "Rand Paul",
+];
+
+let arrayThree = [
+  "Rick Perry",
+  "Marco Rubio",
+  "Bernie Sanders",
+  "Rick Santorum",
+  "Donald Trump",
+  "Scott Walker",
+  "Jim Webb",
+];
+
+let arrayOnePix = [
+  "bush.jpg",
+  "carson.jpg",
+  "chafee.jpg",
+  "christie.jpg",
+  "cruz.jpg",
+  "fiorina.jpg",
+  "gilmore.jpg",
+];
+
+let arrayTwoPix = [
+  "graham.jpg",
+  "jindal.jpg",
+  "kasich.jpg",
+  "lessig.jpg",
+  "omalley.jpg",
+  "pataki.jpg",
+  "paul.jpg",
+];
+
+let arrayThreePix = [
+  "perry.jpg",
+  "rubio.jpg",
+  "sanders.jpg",
+  "santorum.jpg",
+  "trump.jpg",
+  "walker.jpg",
+  "webb.jpg",
+];
