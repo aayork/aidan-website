5b0fb167821501a60cbe1eec9e9bac46ec3ac643
Aidan York <york_aa19@outlook.com>
2023-04-01 23:32:58 -0400

    update

--- nmc/4020/week-10/jukebox/src/script.js
+++ nmc/4020/week-10/jukebox/src/script.js
@@ -34,7 +34,7 @@ export async function redirectToAuthCodeFlow(clientId) {
   params.append("response_type", "code");
   params.append(
     "redirect_uri",
-    "https://aidanayork.com/nmc/4020/jukebox/callback"
+    "https://aidanayork.com/nmc/4020/jukebox/index.html/callback"
   );
   params.append("scope", "user-read-private user-read-email");
   params.append("code_challenge_method", "S256");
@@ -72,7 +72,7 @@ export async function getAccessToken(clientId, code) {
   params.append("code", code);
   params.append(
     "redirect_uri",
-    "https://aidanayork.com/nmc/4020/jukebox/callback"
+    "https://aidanayork.com/nmc/4020/jukebox/index.html/callback"
   );
   params.append("code_verifier", verifier);
 
