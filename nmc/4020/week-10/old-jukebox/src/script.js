4b75997d7fcbb33cab073914510e3a88429bc36c
Aidan York <york_aa19@outlook.com>
2023-04-03 10:40:14 -0400

    update

--- nmc/4020/week-10/old-jukebox/src/script.js
+++ nmc/4020/week-10/old-jukebox/src/script.js
@@ -32,10 +32,7 @@ export async function redirectToAuthCodeFlow(clientId) {
   const params = new URLSearchParams();
   params.append("client_id", clientId);
   params.append("response_type", "code");
-  params.append(
-    "redirect_uri",
-    "https://aidanayork.com/nmc/4020/week-10/jukebox/callback"
-  );
+  params.append("redirect_uri", "http://localhost:5173/callback");
   params.append("scope", "user-read-private user-read-email");
   params.append("code_challenge_method", "S256");
   params.append("code_challenge", challenge);
@@ -70,10 +67,7 @@ export async function getAccessToken(clientId, code) {
   params.append("client_id", clientId);
   params.append("grant_type", "authorization_code");
   params.append("code", code);
-  params.append(
-    "redirect_uri",
-    "https://aidanayork.com/nmc/4020/week-10/jukebox/callback"
-  );
+  params.append("redirect_uri", "http://localhost:5173/callback");
   params.append("code_verifier", verifier);
 
   const result = await fetch("https://accounts.spotify.com/api/token", {
