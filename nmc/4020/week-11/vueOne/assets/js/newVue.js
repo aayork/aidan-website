150f8921dbbb0bc7400196c578135fa48bc61881
Aidan York <york_aa19@outlook.com>
2023-04-03 09:01:42 -0400

    week 11

---
+++ nmc/4020/week-11/vueOne/assets/js/newVue.js
@@ -0,0 +1,12 @@
+var header = new VTTCue({
+  el: "#header",
+  data: {
+    yourName: "Aidan",
+    description: "I drive stick shift.",
+    linkOne: "Introduction",
+    link2: "My Portfolio",
+    link3: "About Me, OK??",
+    link4: "Contact",
+    link5: "my list",
+  },
+});
