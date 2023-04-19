35411e87d8275c7f6e30ae5fac7d5789e281494d
Aidan York <york_aa19@outlook.com>
2023-04-19 11:20:51 -0400

    stuff

--- nmc/4020/week-12/toast/component.js
+++ nmc/4020/week-12/toast/component.js
@@ -1,11 +1,20 @@
-var co = new Vue({
-    el: '#co',
-    data: {
-   toastState: "burnt"
+Vue.component("toast", {
+  props: {
+    toastoption: String,
+    bgcolor: String,
+  },
+  template: `<div><button v-on:click="checkToast" >check the toast</button>
+    <h3 :style="{ backgroundColor: bgcolor}">The toast is {{toastState}} </h3></div>`,
+  data() {
+    return { toastState: "burnt" };
+  },
+  methods: {
+    checkToast() {
+      this.toastState = "perfect";
     },
-     methods: {
-        checkToast(){
-           this.toastState = "perfect"
-        }
-    }
-})
+  },
+});
+
+var co = new Vue({
+  el: "#co",
+});
