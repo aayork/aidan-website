bb1075cdf485ba181ca7249e0ad82796950b6681
Aidan York <113198662+aayork@users.noreply.github.com>
2025-01-21 23:28:15 -0500

    formatting

--- src/app/nmc/4020/page.tsx
+++ src/app/nmc/4020/page.tsx
@@ -27,11 +27,11 @@ export default function NewMediaPortfolio() {
         <PortfolioLink href="week-1/lists.html">
           Lists, Links, and Fonts
         </PortfolioLink>
-        <PortfolioLink href="week-1/form1.html">form 1</PortfolioLink>
-        <PortfolioLink href="week-1/form2.html">form 2</PortfolioLink>
-        <PortfolioLink href="week-1/form3.html">form 3</PortfolioLink>
-        <PortfolioLink href="week-1/form4.html">form 4</PortfolioLink>
-        <PortfolioLink href="week-1/jform.html">jform</PortfolioLink>
+        <PortfolioLink href="/nmc/4020/week-1/form1.html">form 1</PortfolioLink>
+        <PortfolioLink href="/nmc/4020/week-1/form2.html">form 2</PortfolioLink>
+        <PortfolioLink href="/nmc/4020/week-1/form3.html">form 3</PortfolioLink>
+        <PortfolioLink href="/nmc/4020/week-1/form4.html">form 4</PortfolioLink>
+        <PortfolioLink href="/nmc/4020/week-1/jform.html">jform</PortfolioLink>
       </PortfolioSection>
 
       <PortfolioSection title="Week 2">
@@ -39,133 +39,149 @@ export default function NewMediaPortfolio() {
       </PortfolioSection>
 
       <PortfolioSection title="Week 3">
-        <PortfolioLink href="week-3/heart.html">heart</PortfolioLink>
-        <PortfolioLink href="week-3/textstyling.html">
+        <PortfolioLink href="/nmc/4020/week-3/heart.html">heart</PortfolioLink>
+        <PortfolioLink href="/nmc/4020/week-3/textstyling.html">
           text styling
         </PortfolioLink>
-        <PortfolioLink href="week-3/boxEx.html">box exercise</PortfolioLink>
+        <PortfolioLink href="/nmc/4020/week-3/boxEx.html">
+          box exercise
+        </PortfolioLink>
       </PortfolioSection>
 
       <PortfolioSection title="Week 4">
-        <PortfolioLink href="week-4/flexbox0.html">
+        <PortfolioLink href="/nmc/4020/week-4/flexbox0.html">
           Exercises 0 & 2
         </PortfolioLink>
-        <PortfolioLink href="week-4/flexwrap.html">Exercise 1</PortfolioLink>
-        <PortfolioLink href="week-4/flex-align0.html">
+        <PortfolioLink href="/nmc/4020/week-4/flexwrap.html">
+          Exercise 1
+        </PortfolioLink>
+        <PortfolioLink href="/nmc/4020/week-4/flex-align0.html">
           Exercises 3 & 4
         </PortfolioLink>
-        <PortfolioLink href="week-4/basicJavascript/calculate.html">
+        <PortfolioLink href="/nmc/4020/week-4/basicJavascript/calculate.html">
           calculate
         </PortfolioLink>
       </PortfolioSection>
 
       <PortfolioSection title="Week 5">
-        <PortfolioLink href="week-5/candidiates/candidiates.html">
+        <PortfolioLink href="/nmc/4020/week-5/candidiates/candidiates.html">
           Exercise 1
         </PortfolioLink>
-        <PortfolioLink href="week-5/candidiates/candidiates2.html">
+        <PortfolioLink href="/nmc/4020/week-5/candidiates/candidiates2.html">
           Exercise 2
         </PortfolioLink>
-        <PortfolioLink href="week-5/fortune/fortune.html">
+        <PortfolioLink href="/nmc/4020/week-5/fortune/fortune.html">
           Fortune
         </PortfolioLink>
       </PortfolioSection>
 
       <PortfolioSection title="Project 1">
-        <PortfolioLink href="Project-1/index.html">Project 1</PortfolioLink>
+        <PortfolioLink href="/nmc/4020/Project-1/index.html">
+          Project 1
+        </PortfolioLink>
       </PortfolioSection>
 
       <PortfolioSection title="Week 6">
-        <PortfolioLink href="week-6/basicJavascript/conditional.html">
+        <PortfolioLink href="/nmc/4020/week-6/basicJavascript/conditional.html">
           conditional
         </PortfolioLink>
-        <PortfolioLink href="week-6/control/index.html">
+        <PortfolioLink href="/nmc/4020/week-6/control/index.html">
           control CSS
         </PortfolioLink>
-        <PortfolioLink href="week-6/ship/ship.html">spaceship</PortfolioLink>
+        <PortfolioLink href="/nmc/4020/week-6/ship/ship.html">
+          spaceship
+        </PortfolioLink>
       </PortfolioSection>
 
       <PortfolioSection title="ChatGPT Activity">
-        <PortfolioLink href="chatgpt/starter.html">
+        <PortfolioLink href="/nmc/4020/chatgpt/starter.html">
           Cali National Parks slideshow
         </PortfolioLink>
       </PortfolioSection>
 
       <PortfolioSection title="Week 7 & Project 2">
-        <PortfolioLink href="week-7/objects/classlist.html">
+        <PortfolioLink href="/nmc/4020/week-7/objects/classlist.html">
           Object Exercise
         </PortfolioLink>
-        <PortfolioLink href="week-7/custom-objects/custom.html">
+        <PortfolioLink href="/nmc/4020/week-7/custom-objects/custom.html">
           Make an Object Assignment (Project 2)
         </PortfolioLink>
       </PortfolioSection>
 
       <PortfolioSection title="Week 8 & Project 3">
-        <PortfolioLink href="week-8/corona/corona.html">
+        <PortfolioLink href="/nmc/4020/week-8/corona/corona.html">
           Covid Blues (Project 3)
         </PortfolioLink>
-        <PortfolioLink href="week-8/nested/nested.html">
+        <PortfolioLink href="/nmc/4020/week-8/nested/nested.html">
           Nested Exercise
         </PortfolioLink>
-        <PortfolioLink href="week-8/cards/cards.html">
+        <PortfolioLink href="/nmc/4020/week-8/cards/cards.html">
           Cards Exercise
         </PortfolioLink>
       </PortfolioSection>
 
       <PortfolioSection title="Week 9">
-        <PortfolioLink href="week-9/asteroids/asteroids.html">
+        <PortfolioLink href="/nmc/4020/week-9/asteroids/asteroids.html">
           Asteroids
         </PortfolioLink>
-        <PortfolioLink href="week-9/trivia/randomTrivia.html">
+        <PortfolioLink href="/nmc/4020/week-9/trivia/randomTrivia.html">
           Trivia
         </PortfolioLink>
-        <PortfolioLink href="week-9/trivia/randomQuestionsTrivia">
+        <PortfolioLink href="/nmc/4020/week-9/trivia/randomQuestionsTrivia">
           Multiple Random Questions
         </PortfolioLink>
       </PortfolioSection>
 
       <PortfolioSection title="Week 10">
-        <PortfolioLink href="week-10/classmates/classmates.html">
+        <PortfolioLink href="/nmc/4020/week-10/classmates/classmates.html">
           Classmates Assignment
         </PortfolioLink>
-        <PortfolioLink href="week-10/jukebox/index.html">
+        <PortfolioLink href="/nmc/4020/week-10/jukebox/index.html">
           Jamboree Jukebox
         </PortfolioLink>
       </PortfolioSection>
 
       <PortfolioSection title="Week 11">
-        <PortfolioLink href="week-11/vueOne/index.html">
+        <PortfolioLink href="/nmc/4020/week-11/vueOne/index.html">
           Vue Assignment One
         </PortfolioLink>
-        <PortfolioLink href="week-11/VuePartTwo/newVue.html">
+        <PortfolioLink href="/nmc/4020/week-11/VuePartTwo/newVue.html">
           Vue Part Two
         </PortfolioLink>
-        <PortfolioLink href="week-11/VueCats/cats.html">Vue Cats</PortfolioLink>
-        <PortfolioLink href="week-11/VuePartThree/youguys.html">
+        <PortfolioLink href="/nmc/4020/week-11/VueCats/cats.html">
+          Vue Cats
+        </PortfolioLink>
+        <PortfolioLink href="/nmc/4020/week-11/VuePartThree/youguys.html">
           Vue Part 3
         </PortfolioLink>
       </PortfolioSection>
 
       <PortfolioSection title="Week 12">
-        <PortfolioLink href="week-12/toast/component.html">toast</PortfolioLink>
-        <PortfolioLink href="week-12/computed/computed.html">
+        <PortfolioLink href="/nmc/4020/week-12/toast/component.html">
+          toast
+        </PortfolioLink>
+        <PortfolioLink href="/nmc/4020/week-12/computed/computed.html">
           computed
         </PortfolioLink>
-        <PortfolioLink href="week-12/computed2/studentDemo.html">
+        <PortfolioLink href="/nmc/4020/week-12/computed2/studentDemo.html">
           computed 2
         </PortfolioLink>
-        <PortfolioLink href="week-12/dawgs/dog.html">DAWGS API</PortfolioLink>
+        <PortfolioLink href="/nmc/4020/week-12/dawgs/dog.html">
+          DAWGS API
+        </PortfolioLink>
       </PortfolioSection>
 
       <PortfolioSection title="Week 13 & Project 6">
-        <PortfolioLink href="week-13/kitty/kitty.html">cat API</PortfolioLink>
-        <PortfolioLink href="week-13/catshow/catshow.html">
+        <PortfolioLink href="/nmc/4020/week-13/kitty/kitty.html">
+          cat API
+        </PortfolioLink>
+        <PortfolioLink href="/nmc/4020/week-13/catshow/catshow.html">
           Cat Show
         </PortfolioLink>
       </PortfolioSection>
 
       <PortfolioSection title="Final Project">
-        <PortfolioLink href="final-project/mars.html">
+        <PortfolioLink href="/nmc/4020/final-project/mars.html">
           Mars Rover Images
         </PortfolioLink>
       </PortfolioSection>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import { Navigation } from "@/components/Navigation";
 import { Banner } from "@/components/Banner";
 import { About } from "@/components/About";
@@ -8,15 +9,22 @@ import { Footer } from "@/components/Footer";
 
 const Home = () => {
   return (
-    <div>
-      <Navigation />
-      <Banner />
-      <About />
-      <Experience />
-      <Skills />
-      <Blog />
-      <Footer />
-    </div>
+    <>
+      <Head>
+        <title>My Website</title>
+        <meta name="description" content="Welcome to my portfolio website!" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div>
+        <Navigation />
+        <Banner />
+        <About />
+        <Experience />
+        <Skills />
+        <Blog />
+        <Footer />
+      </div>
+    </>
   );
 };
 
