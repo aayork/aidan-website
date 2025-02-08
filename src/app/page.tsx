2ff0b96353e03c95333f626ff6736ce8c888c5d8
Aidan York <113198662+aayork@users.noreply.github.com>
2025-02-08 11:55:01 -0500

    remove apple scroller

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import { Banner } from "@/components/Banner";
 import { About } from "@/components/About";
 import { Experience } from "@/components/Experience";
 import { Skills } from "@/components/Skills";
-import { Projects } from "@/components/Projects";
 import { Footer } from "@/components/Footer";
 
 const Home = () => {
@@ -14,7 +13,6 @@ const Home = () => {
       <About />
       <Experience />
       <Skills />
-      <Projects />
       <Footer />
     </div>
   );
