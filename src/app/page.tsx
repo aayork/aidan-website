e7558ddd544a4d408c8bead1970325eebf856998
Aidan York <113198662+aayork@users.noreply.github.com>
2025-02-08 10:49:43 -0500

    setup aceternity ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import { Banner } from "@/components/Banner";
 import { About } from "@/components/About";
 import { Experience } from "@/components/Experience";
 import { Skills } from "@/components/Skills";
+import { Projects } from "@/components/Projects";
 import { Blog } from "@/components/Blog";
 import { Footer } from "@/components/Footer";
 
