811f7d7b633a89c7c710b512ab275255174d6d93
Aidan York <113198662+aayork@users.noreply.github.com>
2025-02-10 19:34:00 -0500

    stlying changes

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -6,7 +6,7 @@ export const Projects = () => {
     <div className="container mt-10 p-15">
       <h2 className="ds-heading m-10">Projects</h2>
       <div className="grid m-10 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 max-w-[75rem] w-full mx-auto">
-        <Card className="rounded-lg">
+        <Card>
           <CardHeader className="p-0">
             <Image
               src="/images/ritmo.gif"
@@ -19,10 +19,7 @@ export const Projects = () => {
           <CardContent className="p-4">
             <div className="text-xl font-bold">Ritmo!</div>
             <div className="text-sm text-gray-500 dark:text-gray-400">
-              Created 2 days ago
-            </div>
-            <div className="text-sm text-gray-500 dark:text-gray-400">
-              Modified 1 day ago
+              A rhythm game for Apple Vision
             </div>
           </CardContent>
         </Card>
@@ -39,10 +36,7 @@ export const Projects = () => {
           <CardContent className="p-4">
             <div className="text-xl font-bold">The Recipe Box</div>
             <div className="text-sm text-gray-500 dark:text-gray-400">
-              Created 1 day ago
-            </div>
-            <div className="text-sm text-gray-500 dark:text-gray-400">
-              Modified 5 hours ago
+              Share your favorite recipes!
             </div>
           </CardContent>
         </Card>
@@ -59,30 +53,7 @@ export const Projects = () => {
           <CardContent className="p-4">
             <div className="text-xl font-bold">Cinema E-Booking</div>
             <div className="text-sm text-gray-500 dark:text-gray-400">
-              Created 3 days ago
-            </div>
-            <div className="text-sm text-gray-500 dark:text-gray-400">
-              Modified 2 days ago
-            </div>
-          </CardContent>
-        </Card>
-        <Card>
-          <CardHeader className="p-0">
-            <Image
-              src="/images/placeholder.svg"
-              alt="Project"
-              width="400"
-              height="225"
-              className="aspect-video object-cover"
-            />
-          </CardHeader>
-          <CardContent className="p-4">
-            <div className="text-xl font-bold">React Site</div>
-            <div className="text-sm text-gray-500 dark:text-gray-400">
-              Created 4 days ago
-            </div>
-            <div className="text-sm text-gray-500 dark:text-gray-400">
-              Modified 3 days ago
+              Book movies in style 😎
             </div>
           </CardContent>
         </Card>
