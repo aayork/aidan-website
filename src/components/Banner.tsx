256b59a072b540f1dc34944bafcde5e6d7b14f77
Aidan York <113198662+aayork@users.noreply.github.com>
2025-02-10 20:15:35 -0500

    responsive design tests

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -3,24 +3,24 @@ import { AuroraBackground } from "./ui/aurora-background";
 
 export const Banner = () => (
   <AuroraBackground>
-    <div className="ds-banner pt-20 pb-16 m-10 flex items-center">
-      <div className="container flex items-center">
+    <div className="ds-banner pt-20 pb-16 m-10 flex flex-col md:flex-row items-center text-center md:text-left">
+      <div className="container flex flex-col md:flex-row items-center">
         <Image
-          className="rounded-2xl"
+          className="rounded-2xl w-[250px] h-[250px] sm:w-[400px] sm:h-[400px] md:w-[600px] md:h-[600px]"
           src="/images/profile-image.jpg"
           alt="Profile Image"
           width={600}
           height={600}
         />
 
-        <section className="pl-16 pr-24">
-          <h1 className="font-semibold text-4xl leading-[60px] text-black">
+        <section className="mt-8 md:mt-0 md:pl-16 md:pr-24">
+          <h1 className="font-semibold text-3xl sm:text-4xl leading-[40px] sm:leading-[50px] md:leading-[60px] text-black">
             Hi! I’m Aidan, <br />a fourth-year computer science student at UGA.
           </h1>
-          <div className="mt-16">
-            <ul className="flex items-center flex-wrap space-x-16 list-none">
+          <div className="mt-10 sm:mt-16">
+            <ul className="flex flex-col sm:flex-row items-center sm:space-x-16 space-y-4 sm:space-y-0 list-none">
               <li>
-                <strong className="block text-[#ba0c2f] text-3xl font-extrabold">
+                <strong className="block text-[#ba0c2f] text-2xl sm:text-3xl font-extrabold">
                   Lots of
                 </strong>
                 <span className="text-sm tracking-wide">
@@ -28,7 +28,7 @@ export const Banner = () => (
                 </span>
               </li>
               <li>
-                <strong className="block text-[#ba0c2f] text-3xl font-extrabold">
+                <strong className="block text-[#ba0c2f] text-2xl sm:text-3xl font-extrabold">
                   2
                 </strong>
                 <span className="text-sm tracking-wide">Internships</span>
