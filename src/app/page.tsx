e2b0920f7c4fa4bcb6ad41e2851cd3907139d701
Aidan York <113198662+aayork@users.noreply.github.com>
2025-02-13 13:46:53 -0500

    git clean

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,17 +4,20 @@ import { About } from "@/components/About";
 import { Experience } from "@/components/Experience";
 import { Skills } from "@/components/Skills";
 import { Projects } from "@/components/Projects";
+import { Books } from "@/components/Books";
 import { Footer } from "@/components/Footer";
 
 const Home = () => {
   return (
-    <div>
+    <div className="border-[15px] border-green-950">
+      {/* big custom border */}
       <Navigation />
       <Banner />
       <About />
       <Projects />
       <Experience />
       <Skills />
+      <Books />
       <Footer />
     </div>
   );
