025990ec1342dfc9211588dbc0f62f05c0b75d6d
Aidan York <113198662+aayork@users.noreply.github.com>
2025-02-13 12:07:18 -0500

    reset files

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,18 +1,5 @@
 import Link from "next/link";
 
 export const Footer = () => (
-  <footer className="bg-black py-28 text-center">
-    <div className="container mx-auto">
-      <section>
-        <h4 className="text-white text-6xl mb-8">Please reach out!</h4>
-        <Link
-          href="mailto:york_aa19@outlook.com"
-          className="border-2 border-red-800 text-red-800 px-6 py-3 rounded-3xl shadow-md hover:bg-red-800 hover:text-white transition"
-        >
-          EMAIL ME
-        </Link>
-      </section>
-      <p className="mt-8 text-gray-600">&copy;2025 Aidan York</p>
-    </div>
-  </footer>
+  <footer className="bg-black py-28 text-center"></footer>
 );
--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -3,52 +3,5 @@ import Image from "next/image";
 import { Github, Linkedin } from "lucide-react";
 
 export const Navigation = () => (
-  <header className="fixed top-0 left-0 right-0 z-50 flex justify-between items-center bg-white px-10 py-8 transition-all duration-400 ease-in-out shadow-md rounded-xl m-2 max-w-[100rem]">
-    <Link href="/">
-      <p className="uppercase text-lg tracking-widest text-black flex items-center">
-        Aidan York
-      </p>
-    </Link>
-    <ul className="list-none flex items-center flex-wrap">
-      <li className="ml-3 first:ml-0 transition-all">
-        <Link
-          href="https://medium.com/@York_aa19"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <div className="w-8 h-8 bg-[#ba0c2f] hover:bg-black transition rounded-full flex items-center justify-center">
-            <Image
-              priority
-              src="/images/medium.svg"
-              width={16}
-              height={16}
-              alt="Follow me on Medium"
-            />
-          </div>
-        </Link>
-      </li>
-      <li className="ml-3 first:ml-0 transition-all">
-        <Link
-          href="https://github.com/aayork"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <div className="w-8 h-8 bg-[#ba0c2f] hover:bg-black transition rounded-full flex items-center justify-center">
-            <Github className="w-5 h-5 text-white" />
-          </div>
-        </Link>
-      </li>
-      <li className="ml-3 first:ml-0 transition-all">
-        <Link
-          href="https://www.linkedin.com/in/aidan-york/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <div className="w-8 h-8 bg-[#ba0c2f] hover:bg-black transition rounded-full flex items-center justify-center">
-            <Linkedin className="w-5 h-5 text-white" />
-          </div>
-        </Link>
-      </li>
-    </ul>
-  </header>
+  <header className="fixed top-0 left-0 right-0 z-50 flex justify-between items-center bg-white px-10 py-8 transition-all duration-400 ease-in-out shadow-md rounded-xl m-2 max-w-[100rem]"></header>
 );
